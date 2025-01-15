ae10259719b54203bde67ee30199435b3ab46d8f
aaaneel <nizikenovaanel@gmail.com>
2025-01-15 16:43:34 +0500

    Initial commit

---
+++ src/main/java/com/example/dzn/DznApplication.java
@@ -0,0 +1,13 @@
+package com.example.dzn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DznApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DznApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dzn/DznApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dzn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DznApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
